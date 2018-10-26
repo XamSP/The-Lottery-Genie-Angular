97b323dec449b298abc7ae7729d3831876a16cae
Alexander Teodor-Mazilu <Amazilu77@gmail.com>
2018-10-25 22:19:30 -0400

    styling lamp page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,3 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'The-Lottery-Genie-Angular';
 }
-// <script>
-// let lamp2 = document.getElementById('lamp');
-
-// lamp2.addEventListener('mouseover', (event) => {
-//   alert("The Genie has been been summoned!")
-//   location.href = '/auth/login'
-// });</script>
