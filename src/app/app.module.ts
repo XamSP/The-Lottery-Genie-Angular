e303fcf7520cf2a56e6510b9b23c1803be04a069
Alexander Teodor-Mazilu <Amazilu77@gmail.com>
2018-10-27 22:55:01 -0400

    fixed double binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { MyGenieNameComponent } from './my-genie-name/my-genie-name.component';
+import { MyGenieName } from './my-genie-name/my-genie-name.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyGenieNameComponent
+    MyGenieName,
   ],
   imports: [
     BrowserModule,
