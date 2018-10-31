f7c5e184bb2f6ef38da1f38bf1f34ba86f95add2
Alexander Teodor-Mazilu <Amazilu77@gmail.com>
2018-10-31 00:12:54 -0400

    fixed signup form

--- src/app/my-signup-form/my-signup-form.component.ts
+++ src/app/my-signup-form/my-signup-form.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-my-signup-form',
   templateUrl: './my-signup-form.component.html',
-  styleUrls: ['./my-signup-form.component.css']
+  styleUrls: ['./my-signup-form.component.scss']
 })
 export class MySignupFormComponent implements OnInit {
 
