562dcfe8e211237e9185fb1eba957b47c5d3f02a
Alexander Teodor-Mazilu <Amazilu77@gmail.com>
2018-10-30 23:56:25 -0400

    broken login link

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { MyGenieName } from './my-genie-name/my-genie-name.component';
+import { MySignupFormComponent } from './my-signup-form/my-signup-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MyGenieName,
+    MySignupFormComponent,
   ],
   imports: [
     BrowserModule,
